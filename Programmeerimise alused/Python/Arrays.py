be9ffc892f5a1ab74add81ae1bfae5e6ed1c74da
kaurilves <95514872+kaurilves@users.noreply.github.com>
2024-02-04 17:17:16 +0200

    Delete Programmeerimise alused directory

--- Programmeerimise alused/Python/Arrays.py
+++
@@ -1,6 +0,0 @@
-
-names = ["Kaur", "Siim", "Taavi", "Liisa", "Martin", "Aadu", "Kalev", "Mikk", "Peeter", "Anna"]
-for i, text in enumerate(names):
-    if not (i % 2):
-        print(text)
-
--- Programmeerimise alused/praktikum3/Task4.py
+++
@@ -1,5 +0,0 @@
-"""Ülesanne 4 Isikukood (revisited)
-Kuidas lisada kuude nimed? Kuidas leida kontrollnumbrit?"""
-
-# Ülesanne 4. Isikukood (revisited)
-# Kuidas lisada kuude nimed? Kuidas leida kontrollnumbrit?
